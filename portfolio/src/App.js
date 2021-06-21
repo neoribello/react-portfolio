9c554b262f8d1e386e830be6917889f3c399a7e4
neoribello <neil.oribello@gmail.com>
2021-06-21 11:53:05 -0700

    added sass and new header component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello React</h1>
+      <Header />
     </div>
   );
 }
---
+++ portfolio/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Header/Header.scss'
+
+function Header(props) {
+  return (
+    <div>
+      <h1>This is the header</h1>
+    </div>
+  );
+}
+
+export default Header;

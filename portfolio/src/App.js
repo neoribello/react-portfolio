dcd50c50dd5a4249858951ecb51790031398312b
neoribello <neil.oribello@gmail.com>
2021-06-28 16:19:00 -0700

    trying to retrieve single data

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Header from './components/Header/Header.jsx';
 import Home from './components/Home/Home.jsx';
 import Portfolio from './components/Portfolio/Portfolio.jsx';
+import ProjectDetails from './components/ProjectDetails/ProjectDetails';
 import About from './components/About/About.jsx';
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
         <Header />
         <Switch>
           <Route path="/" active="home" exact component={Portfolio} />
-          <Route path="/portfolio" active="portfolio"  component={Portfolio} />
+          <Route path="/" component={ProjectDetails} />
+          <Route path="/portfolio/:projectId" active="portfolio" component={Portfolio} />
           <Route path="/about" active="about" component={About} />
         </Switch>
       </div>
--- portfolio/src/components/ProjectDetails/ProjectDetails.jsx
+++ portfolio/src/components/ProjectDetails/ProjectDetails.jsx
@@ -3,12 +3,36 @@ import db from '../../firebase';
 import { useParams } from "react-router-dom";
 
 function ProjectDetails(props) {
-  const [project, setProjects] = useState([]);
+  const [project, setProject] = useState([]);
 
+  const fetchProjects = async() => {
+    const response = db.collection('projects');
+    const data = await response.get();
+
+    data.docs.forEach(item => {
+      console.log("data", item.data)
+      setProject([...project, item.data()])
+    })
+  }
+
+  useEffect(() => {
+    fetchProjects();
+  }, [])
+
+  console.log("project", project)
 
   return (
     <div>
-    </div>
+    {
+      project && project.map(p =>{
+        return(
+          <div className="blog-container">
+            <h4>{p.projectName}</h4>
+          </div>
+        )
+      })
+    }
+  </div>
   );
 }
 

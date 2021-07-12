3249d9ac235007e2c61262d4f28e1b7f3f50f961
neoribello <neil.oribello@gmail.com>
2021-07-12 15:09:45 -0700

    toggle class on scorll

--- portfolio/src/components/Navigation/Navigation.jsx
+++ portfolio/src/components/Navigation/Navigation.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 import './Navigation.scss'
 
 function Navigation({ active }) {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () =>
+      setScroll(window.pageYOffset > 10)
+      );
+    }
+  }, []);
+
   return (
-    <div className="navigation">
+    <div className={`navigation ${scroll ? "scroll" : ""}`}>
       <ul className="navigation-links__container">
         <div className="navigation-home">
           <Link to='/'>

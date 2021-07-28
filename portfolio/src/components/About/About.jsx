c84a2cbe243120ffd972c28e7626c0efe123dc1e
neoribello <neil.oribello@gmail.com>
2021-07-28 16:43:47 -0700

    added about section

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './About.scss';
+import ProfilePic from '../../Assets/img/profile-pic.png';
 
 export default function About(props) {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <section className="about-container">
+      <div className="about-content">
+        <h2>Hi I'm Neil, lets get to work</h2>
+        <img src={ProfilePic} alt="about-img"/>
+          <p>I'm Neil Oribello, a Vancouver based Web Developer. Just a regular guy who likes making weird things with web technologies. Proficient with HTML, CSS(SASS and POSTCSS), JavaScript and JS Frameworks, with knowledge of User Interface and Experience Design.</p>
+      </div>
+    </section>
   );
 }
--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Portfolio from '../Portfolio/Portfolio';
 import Header from '../Header/Header';
 import Skills from '../Skills/Skills';
+import About from '../About/About';
 import '../Home/Home.scss'
 
 export default function Home(props) {
   return (
     <div className="homepage">
       <Header />
+      <About />
       <div className="background">
         <Skills />
         <Portfolio />

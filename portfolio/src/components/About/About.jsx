dfc2f0942c1908a24d9d1b93f07286936e09d2ac
neoribello <neil.oribello@gmail.com>
2021-07-29 22:27:49 -0700

    daily commits

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -6,9 +6,15 @@ export default function About(props) {
   return (
     <section className="about-container">
       <div className="about-content">
-        <h2>Hi I'm Neil, lets get to work</h2>
         <img src={ProfilePic} alt="about-img"/>
-          <p>I'm Neil Oribello, a Vancouver based Web Developer. Just a regular guy who likes making weird things with web technologies. Proficient with HTML, CSS(SASS and POSTCSS), JavaScript and JS Frameworks, with knowledge of User Interface and Experience Design.</p>
+          <div className="about-text">
+            <h2>
+              Let's get to work
+            </h2>
+            <p>
+              A Vancouver based Web Developer. Just a regular guy who likes making things with web technologies. Proficient with HTML, CSS(SASS and POSTCSS), JavaScript and JS Frameworks, with knowledge of User Interface and Experience Design.
+            </p>
+          </div>
       </div>
     </section>
   );
